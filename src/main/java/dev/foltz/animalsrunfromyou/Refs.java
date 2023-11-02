80022cc001aa337e3ef590e7126037e5e21540d6
unknown <sam.foltz@hotmail.com>
2023-11-01 19:02:06 -0700

    Tweaked some distances/speeds

--- src/main/java/dev/foltz/animalsrunfromyou/Refs.java
+++ src/main/java/dev/foltz/animalsrunfromyou/Refs.java
@@ -1,19 +1,19 @@
 package dev.foltz.animalsrunfromyou;
 
 public abstract class Refs {
-    public static final float CHICKEN_FEAR_RANGE = 12.0f;
-    public static final float CHICKEN_SPEED_FAST = 1.4f;
+    public static final float CHICKEN_FEAR_RANGE = 32.0f;
+    public static final float CHICKEN_SPEED_FAST = 1.8f;
     public static final float CHICKEN_SPEED_SLOW = 1.2f;
 
-    public static final float COW_FEAR_RANGE = 24.0f;
-    public static final float COW_SPEED_FAST = 1.4f;
-    public static final float COW_SPEED_SLOW = 1.0f;
+    public static final float COW_FEAR_RANGE = 64.0f;
+    public static final float COW_SPEED_FAST = 2f;
+    public static final float COW_SPEED_SLOW = 1.4f;
 
-    public static final float PIG_FEAR_RANGE = 20.0f;
-    public static final float PIG_SPEED_FAST = 1.4f;
-    public static final float PIG_SPEED_SLOW = 1.1f;
+    public static final float PIG_FEAR_RANGE = 48.0f;
+    public static final float PIG_SPEED_FAST = 2f;
+    public static final float PIG_SPEED_SLOW = 1.4f;
 
-    public static final float SHEEP_FEAR_RANGE = 16.0f;
-    public static final float SHEEP_SPEED_FAST = 1.3f;
-    public static final float SHEEP_SPEED_SLOW = 1.0f;
+    public static final float SHEEP_FEAR_RANGE = 48.0f;
+    public static final float SHEEP_SPEED_FAST = 2f;
+    public static final float SHEEP_SPEED_SLOW = 1.4f;
 }
